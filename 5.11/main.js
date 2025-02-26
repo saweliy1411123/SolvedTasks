fbd50d340d4f59079c3b2c4074f32478274eb5f9
saweliy1411123 <saweliy1411@gmail.com>
2025-02-26 15:42:26 +0500

    ещё осталось одну дорешать

---
+++ 5.11/main.js
@@ -0,0 +1,78 @@
+let date = new Date(2012, 1, 20, 3, 12);
+alert(date);
+
+let date = new Date();
+
+function getWeekDay(date) {
+  switch (date.getDay()) {
+    case 1:
+      return "ПН";
+    case 2:
+      return "ВТ";
+    case 3:
+      return "СР";
+    case 4:
+      return "ЧТ";
+    case 5:
+      return "ПТ";
+    case 6:
+      return "СБ";
+    case 7:
+      return "ВС";
+  }
+}
+
+// function getWeekDay(date){
+//   let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+//   return days[date.getDay()];
+// }
+alert(getWeekDay(date));
+
+let date = new Date(2012, 0, 3);
+
+function getWeekDay(date) {
+  return date.getDay();
+}
+alert(getWeekDay(date));
+
+let date = new Date(2015, 0, 2);
+
+function getDateAgo(date, day) {
+  let s = new Date(date);
+  s.setDate(date.getDate() - day);
+  return s.getDate();
+}
+
+console.log(getDateAgo(date, 2));
+
+let date = new Date(2012, 1);
+
+function getDateAgo(date) {
+  let s = new Date(date.getFullYear(), date.getMonth() + 1);
+  s.setDate(s.getDate() - 1);
+  return s.getDate();
+}
+
+console.log(getDateAgo(date));
+
+function getSecondsToday() {
+  let s = new Date();
+  let h = s.getHours();
+  let m = s.getMinutes();
+  let sec = s.getSeconds();
+  return h * 60 * 60 + m * 60 + sec;
+}
+
+console.log(getSecondsToday());
+// alert(getSecondsToday())
+
+function getSecondsToday() {
+  let s = new Date();
+  let h = s.getHours();
+  let m = s.getMinutes();
+  let sec = s.getSeconds();
+  return 24 * 60 * 60 - (h * 60 * 60 + m * 60 + sec);
+}
+
+console.log(getSecondsToday());
+// alert(getSecondsToday())
