039f306c8e44b642c8a7a55eba386e7b0de3fc67
saweliy1411123 <saweliy1411@gmail.com>
2025-05-05 12:13:33 +0500

    2.2 не получается запустить, не знаю почему

---
+++ Angular/2.1/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+      
+@Component({
+    selector: "my-app",
+    standalone: true,
+    template: "<h1>Hello METANIT.COM</h1>"
+})
+export class AppComponent {}
